61de49ce621efca899233c2edabeec583e97e401
robobro <cbbolocan@gmail.com>
2019-07-19 14:16:06 -0400

    bruijhjhh

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class Home extends Component {
   render() {
-    return <div>DEV react-frontend-starter!</div>
+    return <div>bruh web starter</div>
   }
 }
 
